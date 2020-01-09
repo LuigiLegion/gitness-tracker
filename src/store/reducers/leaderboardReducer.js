ca16d7539357a9723bcbd1a63608ebdf60c74257
Tal Luigi <luigi.t.agr@gmail.com>
2020-01-09 00:32:56 -0500

    feat: created organizations fetching action, action creator, thunk creator, and reducer case in the leaderboard reducer

--- src/store/reducers/leaderboardReducer.js
+++ src/store/reducers/leaderboardReducer.js
@@ -18,18 +18,52 @@ const initialState = {
 };
 
 // Action Types
+const GOT_ORGANIZATIONS = 'GOT_ORGANIZATIONS';
 const CLEARED_ALL_DATA = 'CLEARED_ALL_DATA';
 
 // Action Creators
+export const gotOrganizationsActionCreator = organizations => ({
+  type: GOT_ORGANIZATIONS,
+  organizations,
+});
+
 export const clearedAllDataActionCreator = () => ({
   type: CLEARED_ALL_DATA,
 });
 
 // Thunk Creators
+export const getOrganizationsThunkCreator = username => {
+  return async (dispatch, getState, { getFirestore }) => {
+    try {
+      const customQuery = organizationsQueryGenerator(username);
+
+      const { data } = await githubDataFetcher(customQuery);
+
+      const organizations = data.user.organizations.nodes;
+
+      // console.log(
+      //   'organizations in getOrganizationsThunkCreator: ',
+      //   organizations
+      // );
+
+      dispatch(gotOrganizationsActionCreator(organizations));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
 
 // Reducer
 const leaderboardReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GOT_ORGANIZATIONS:
+      // console.log('action.organizations in GOT_ORGANIZATIONS: ', action.organizations);
+
+      return {
+        ...state,
+        organizations: [...action.organizations],
+      };
+
     case CLEARED_ALL_DATA:
       return {
         ...state,
