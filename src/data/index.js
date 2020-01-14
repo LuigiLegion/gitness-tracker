ce253f65a179923e98f973267af77b6f76f57eb9
Tal Luigi <luigi.t.agr@gmail.com>
2020-01-14 03:06:53 -0500

    chore: imported the contributorsSorter into /src/data/index.js and exported it as part of an object for easier imports in other files

--- src/data/index.js
+++ src/data/index.js
@@ -4,6 +4,7 @@ const teamsQueryGenerator = require('./queries/teamsQueryGenerator');
 const organizationContributorsQueryGenerator = require('./queries/organizationContributorsQueryGenerator');
 const teamContributorsQueryGenerator = require('./queries/teamContributorsQueryGenerator');
 const githubDataFetcher = require('./fetchers/githubDataFetcher');
+const contributorsSorter = require('./sorters/contributorsSorter');
 
 // Exports
 module.exports = {
@@ -12,4 +13,5 @@ module.exports = {
   organizationContributorsQueryGenerator,
   teamContributorsQueryGenerator,
   githubDataFetcher,
+  contributorsSorter,
 };
