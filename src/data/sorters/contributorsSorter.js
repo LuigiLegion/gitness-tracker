33886659b3d00239d37096464cdc5cace4a09989
Tal Luigi <luigi.t.agr@gmail.com>
2020-01-14 03:05:42 -0500

    feat: created the contributorsSorter function as a sorter and moved thunk creator contributors sorting logic to it

---
+++ src/data/sorters/contributorsSorter.js
@@ -0,0 +1,19 @@
+const contributorsSorter = contributors => {
+  contributors.sort((contributorOne, contributorTwo) => {
+    if (
+      contributorOne.node.contributionsCollection.totalCommitContributions >
+      contributorTwo.node.contributionsCollection.totalCommitContributions
+    ) {
+      return -1;
+    } else if (
+      contributorOne.node.contributionsCollection.totalCommitContributions <
+      contributorTwo.node.contributionsCollection.totalCommitContributions
+    ) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
+
+module.exports = contributorsSorter;
