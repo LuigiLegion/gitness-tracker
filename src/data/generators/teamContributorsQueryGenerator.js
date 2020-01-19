995c2f4f6a43cf7e00c3f36a21e51707d05d052c
Tal Luigi <luigi.t.agr@gmail.com>
2020-01-18 20:08:21 -0500

    feat: modified the teamContributors query generator to support cursors by using the first, after, and cursor parameters

--- src/data/generators/teamContributorsQueryGenerator.js
+++ src/data/generators/teamContributorsQueryGenerator.js
@@ -2,14 +2,16 @@
 const teamContributorsQueryGenerator = (
   organizationLogin,
   teamSlug,
+  cursor,
   timeISO
 ) => {
   const teamContributorsQuery = `
     {
       organization(login: "${organizationLogin}") {
         team(slug: "${teamSlug}") {
-          members {
+          members(first: 25${cursor ? `, after: "${cursor}"` : ''}) {
             edges {
+              cursor,
               node {
                 id,
                 login,
