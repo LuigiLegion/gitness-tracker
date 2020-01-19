6a6d593bf38af48af2418ad8ad978f07a15e1274
Tal Luigi <luigi.t.agr@gmail.com>
2020-01-19 02:33:05 -0500

    update: removed the organization leaderboard generation logic from the GenerateTeams component

--- src/components/leaderboard/GenerateTeams.js
+++ src/components/leaderboard/GenerateTeams.js
@@ -5,10 +5,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import {
-  getTeamsThunkCreator,
-  getOrganizationContributorsThunkCreator,
-} from '../../store/reducers/leaderboardReducer';
+import { getTeamsThunkCreator } from '../../store/reducers/leaderboardReducer';
 
 // Component
 export class GenerateTeams extends Component {
@@ -17,12 +14,10 @@ export class GenerateTeams extends Component {
 
     this.state = {
       organization: '',
-      organizationTime: '0',
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleGenerate = this.handleGenerate.bind(this);
   }
 
   handleChange(event) {
@@ -47,22 +42,6 @@ export class GenerateTeams extends Component {
     // console.log('getTeamsThunk in GenerateTeams handleSubmit: ', getTeamsThunk);
 
     getTeamsThunk(organization);
-  }
-
-  handleGenerate(event) {
-    // console.log('event.target.id: ', event.target.id);
-    // console.log('event.target.value: ', event.target.value);
-
-    event.preventDefault();
-
-    const { organization, organizationTime } = this.state;
-    const { getOrganizationContributorsThunk } = this.props;
-
-    // console.log('organization in GenerateTeams handleSubmit: ', organization);
-    // console.log('organizationTime in GenerateTeams handleSubmit: ', organizationTime);
-    // console.log('getOrganizationContributorsThunk in GenerateTeams handleSubmit: ', getOrganizationContributorsThunk);
-
-    getOrganizationContributorsThunk(organization, organizationTime);
   }
 
   render() {
@@ -117,49 +96,6 @@ export class GenerateTeams extends Component {
             >
               Generate
             </button>
-
-            <hr />
-
-            <span className="card-title">
-              <span className="gray-text-color bold-text-style">
-                Generate Org Leaderboard
-              </span>
-            </span>
-
-            <div className="input-field col s12">
-              <label htmlFor="time">
-                Time<span className="red-text-color">*</span>
-              </label>
-
-              <br />
-              <br />
-
-              <select
-                id="organizationTime"
-                className="browser-default"
-                required
-                onChange={this.handleChange}
-              >
-                <option value="0">Choose Time</option>
-                <option value="2629746000">Past Month</option>
-                <option value="7889238000">Past 3 Months</option>
-                <option value="15778476000">Past 6 Months</option>
-                <option value="31556952000">Past Year</option>
-              </select>
-            </div>
-
-            <br />
-
-            <button
-              className="btn black lighten-1 z-depth-0"
-              disabled={
-                !this.state.organization.length ||
-                !Number(this.state.organizationTime)
-              }
-              onClick={this.handleGenerate}
-            >
-              Generate
-            </button>
           </form>
         </div>
       </div>
@@ -176,9 +112,6 @@ const mapDispatchToProps = dispatch => ({
   getTeamsThunk(organizationLogin) {
     dispatch(getTeamsThunkCreator(organizationLogin));
   },
-  getOrganizationContributorsThunk(organizationLogin, time) {
-    dispatch(getOrganizationContributorsThunkCreator(organizationLogin, time));
-  },
 });
 
 export default connect(
@@ -190,5 +123,4 @@ export default connect(
 GenerateTeams.propTypes = {
   organizations: PropTypes.array,
   getTeamsThunk: PropTypes.func,
-  getOrganizationContributorsThunk: PropTypes.func,
 };
