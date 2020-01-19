904d40c5764703f1e8764f9d71447a5935881bf5
Tal Luigi <luigi.t.agr@gmail.com>
2020-01-18 22:50:00 -0500

    update: swapped Github Repo for Source in the link leading to the github repo in the Links and LinksBurger components

--- src/components/layout/Links.js
+++ src/components/layout/Links.js
@@ -22,7 +22,7 @@ const Links = props => {
           rel="noopener noreferrer"
           className="gray-text-color"
         >
-          <span className="gray-text-color bold-text-style">GitHub Repo</span>
+          <span className="gray-text-color bold-text-style">Source</span>
         </a>
       </li>
 
