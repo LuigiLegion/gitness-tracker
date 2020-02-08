acc9c577ea180ed03ab991c338ca2e44bfc19cd8
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-08 02:09:22 -0500

    feat: refactored the SelectOrganization class component into a stateful functional component using hooks

--- src/components/leaderboard/SelectOrganization.js
+++ src/components/leaderboard/SelectOrganization.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/button-has-type */
 
 // Imports
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -9,101 +9,101 @@ import {
   getTeamsThunkCreator,
   gotOrganizationLoginActionCreator,
 } from '../../store/reducers/leaderboardReducer';
+import { usePrevious } from '../../helpers/index';
 
 // Component
-class SelectOrganization extends PureComponent {
-  state = {
-    organizationLogin: '',
-  };
+const SelectOrganization = ({
+  userLogin,
+  organizations,
+  organizationLogin,
+  getTeamsThunk,
+  gotOrganizationLoginAction,
+}) => {
+  const [selectedOrganizationLogin, setSelectedOrganizationLogin] = useState(
+    ''
+  );
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevProps.userLogin !== this.props.userLogin &&
-      prevState.organizationLogin
-    ) {
-      this.setState({
-        organizationLogin: '',
-      });
-      this.props.gotOrganizationLoginAction('');
+  const prevOrganizationLogin = usePrevious(selectedOrganizationLogin);
+
+  useEffect(() => {
+    if (prevOrganizationLogin) {
+      setSelectedOrganizationLogin('');
+      document.getElementById('organizationLogin').value = '';
+      gotOrganizationLoginAction('');
     }
-  }
+    // eslint-disable-next-line
+  }, [userLogin]);
 
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value,
-    });
+  const handleChange = event => {
+    setSelectedOrganizationLogin(event.target.value);
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    this.props.getTeamsThunk(this.state.organizationLogin);
+    getTeamsThunk(selectedOrganizationLogin);
   };
 
-  render() {
-    const { organizations, organizationLogin } = this.props;
+  return (
+    <div className="container center">
+      <div className="section center">
+        <form onSubmit={handleSubmit} className="card white center">
+          <span className="card-title">
+            <span className="gray-text-color bold-text-style">Org</span>
+          </span>
 
-    return (
-      <div className="container center">
-        <div className="section center">
-          <form onSubmit={this.handleSubmit} className="card white center">
-            <span className="card-title">
-              <span className="gray-text-color bold-text-style">Org</span>
-            </span>
-
-            <div className="input-field col s12">
-              <label htmlFor="organizationLogin">
-                Orgs<span className="red-text-color">*</span>
-              </label>
-
-              <br />
-              <br />
-
-              <select
-                id="organizationLogin"
-                className="browser-default"
-                required
-                onChange={this.handleChange}
-              >
-                <option value="">Select Org</option>
-
-                {organizations.length
-                  ? organizations.map(curOrganization => (
-                      <option
-                        key={curOrganization.id}
-                        value={curOrganization.login}
-                      >
-                        {curOrganization.login}
-                      </option>
-                    ))
-                  : null}
-              </select>
-            </div>
+          <div className="input-field col s12">
+            <label htmlFor="organizationLogin">
+              Orgs<span className="red-text-color">*</span>
+            </label>
 
             <br />
+            <br />
 
-            <button
-              className="btn waves-effect waves-light black lighten-1 z-depth-0"
-              disabled={!this.state.organizationLogin.length}
+            <select
+              id="organizationLogin"
+              className="browser-default"
+              required
+              onChange={handleChange}
             >
-              Select
-            </button>
+              <option value="">Select Org</option>
 
-            <br />
-            <br />
+              {organizations.length
+                ? organizations.map(curOrganization => (
+                    <option
+                      key={curOrganization.id}
+                      value={curOrganization.login}
+                    >
+                      {curOrganization.login}
+                    </option>
+                  ))
+                : null}
+            </select>
+          </div>
 
-            <span className="italic-text-style">
-              {organizationLogin ? organizationLogin : 'Not Yet Selected'}
-            </span>
+          <br />
 
-            <br />
-            <br />
-          </form>
-        </div>
+          <button
+            className="btn waves-effect waves-light black lighten-1 z-depth-0"
+            disabled={!selectedOrganizationLogin.length}
+          >
+            Select
+          </button>
+
+          <br />
+          <br />
+
+          <span className="italic-text-style">
+            {organizationLogin ? organizationLogin : 'Not Yet Selected'}
+          </span>
+
+          <br />
+          <br />
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 // Container
 const mapStateToProps = state => ({
