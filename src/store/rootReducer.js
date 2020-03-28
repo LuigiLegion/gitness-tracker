c82c74110f40a1596909f1b10fadb3360bccb40d
Tal Luigi <luigi.t.agr@gmail.com>
2020-03-28 16:57:09 -0400

    chore: added code block categorization in rootReducer, src/index.js, and src/store/index.js

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,11 @@
+// Imports
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
 
 import leaderboardReducer from './reducers/leaderboardReducer';
 
+// Initializations
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer,
