aeed6a018997e7a944a0d4aca3c55f16c392439a
Tal Luigi <luigi.t.agr@gmail.com>
2021-07-24 22:37:19 -0400

    feat: refactored the Dashboard component to use the MadeBy component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,6 +7,7 @@ import {
   TeamSelect,
   ContributorsGenerate,
   ContributorsDisplay,
+  MadeBy,
 } from '..';
 import { generateStyle, displayStyle } from '../../styles';
 
@@ -39,17 +40,7 @@ const Dashboard = () => {
           </div>
         </div>
 
-        <div className="text-align-center padding-bottom">
-          {'Made with ❤ by '}
-
-          <a
-            href="https://www.linkedin.com/in/tal-luigi"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="text-style-bold">Tal Luigi</span>
-          </a>
-        </div>
+        <MadeBy />
       </div>
     </div>
   );
