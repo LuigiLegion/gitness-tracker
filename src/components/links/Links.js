e699da730e196a968e3fff3ab31233173e2efe29
Tal Luigi <luigi.t.agr@gmail.com>
2022-04-11 23:23:26 -0400

    chore: removed nested span elements across NavLink and anchor elements in the Links component

--- src/components/links/Links.js
+++ src/components/links/Links.js
@@ -7,30 +7,33 @@ const Links = () => {
   return (
     <ul className="right">
       <li>
-        <NavLink to="/about" className="text-color-gray">
-          <span className="text-color-gray text-style-bold">About</span>
+        <NavLink
+          className="text-color-gray text-style-bold"
+          to="/about"
+        >
+          About
         </NavLink>
       </li>
 
       <li>
         <a
-          className="text-color-gray"
+          className="text-color-gray text-style-bold"
           href="https://github.com/LuigiLegion/gitness-tracker"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="text-color-gray text-style-bold">Source</span>
+          Source
         </a>
       </li>
 
       <li>
         <a
-          className="text-color-gray"
+          className="text-color-gray text-style-bold padding-right"
           href="https://taluigi.netlify.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="text-color-gray text-style-bold">Contact</span>
+          Contact
         </a>
       </li>
     </ul>
