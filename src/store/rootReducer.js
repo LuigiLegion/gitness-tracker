f7db12fad2b1289bb8b0a7254fdb2c706505fa56
Tal Luigi <luigi.t.agr@gmail.com>
2022-04-14 21:58:48 -0400

    feat: set layoutReducer as layout in the combineReducers invocation in rootReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -4,12 +4,14 @@ import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
 
 import leaderboardReducer from './reducers/leaderboardReducer';
+import layoutReducer from './reducers/layoutReducer';
 
 // Initializations
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer,
   leaderboard: leaderboardReducer,
+  layout: layoutReducer,
 });
 
 // Exports
